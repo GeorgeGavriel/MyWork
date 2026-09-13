720dd04006ece6a8ac5b66f3c181422df0f977fe
George Gavriel <ggavri02@ucy.ac.cy>
2026-09-13 13:44:46 +0300

    added new lines to myFirstProgram.c to express excitement about learning C programming and commitment to practice and improve skills.

--- myFirstProgram.c
+++ myFirstProgram.c
@@ -3,5 +3,8 @@
 int main() {
     printf("Hello, World!\n");
     printf("Hello im George Gavriel and this is my first program in C23 language standard version.\n");
+
+    printf("I am excited to learn more about C programming and its features.\n");
+    printf("I will continue to practice and improve my skills in C programming.\n");
     return 0;
 }
