8e2b617182ce16b9d36d97351a6c5583b914160e
George Gavriel <ggavri02@ucy.ac.cy>
2026-09-13 13:49:51 +0300

    i have added some additional print statements to my first C program to express my excitement about learning C programming and to thank the reader for taking the time to read my program.

--- myFirstProgram.c
+++ myFirstProgram.c
@@ -6,5 +6,8 @@ int main() {
 
     printf("I am excited to learn more about C programming and its features.\n");
     printf("I will continue to practice and improve my skills in C programming.\n");
+
+    printf("Thank you for taking the time to read my first program in C23 language standard version.\n");
+    printf("I look forward to learning more and creating more programs in the future.\n");
     return 0;
 }
