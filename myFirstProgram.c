b0c97f27acc7fab9791092041f639caae1226b66
George Gavriel <ggavri02@ucy.ac.cy>
2026-09-13 13:37:51 +0300

    first commit

---
+++ myFirstProgram.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello im George Gavriel and this is my first program in C23 language standard version.\n");
+    return 0;
+}
